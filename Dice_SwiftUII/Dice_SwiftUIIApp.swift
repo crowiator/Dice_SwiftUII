249e48403b903d5b49dbdc7905b16adab5785154
Samuel Vrana <vrana.samuel@gmail.com>
2023-03-26 09:50:54 +0200

    Initial Commit

---
+++ Dice_SwiftUII/Dice_SwiftUIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIIApp.swift
+//  Dice_SwiftUII
+//
+//  Created by crow on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
